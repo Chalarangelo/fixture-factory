122da300e1c56be57c731becfa974ee57f23da0a
Chalarangelo <chalarangelo@gmail.com>
2021-05-10 22:11:37 +0300

    Create FactoryDefinition

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const Traits = Symbol('FixtureFactory.__Traits');
+export const TraitDefinitions = Symbol('FixtureFactory.__TraitDefinitions');
+export const FactoryInitializer = Symbol('FixtureFactory.__FactoryInitializer');
+export const FactoryName = Symbol('FixtureFactory.__FactoryName');
